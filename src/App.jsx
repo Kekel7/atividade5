e9cc3b7ff82a2102d5ed48da2543de770cc06a98
kekel7 <kellmys.sz.ribeiro@gmail.com>
2022-07-03 16:12:39 -0300

    listas

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App(){
+
+return(   
+    <div>
+        <ol className="lista">
+              <li>Tools</li>
+              <li>Pricing</li>
+              <li>Blog</li>
+              <li>About</li>
+              <li>Customers</li>
+            </ol>
+    </div>
+)
+}
+export default App;
